a73ef1ca4466d432fb8d51a7d0022347b427583e
jesslynowen7 <owenjesslyn@gmail.com>
2023-10-27 02:01:47 +0700

    first commit

---
+++ Backend/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const authRoutes = require('./src/auth');
+
+// Middlewares
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// API routes
+app.use('/auth', authRoutes);
+// app.use('/', modelRoutes);
+// app.use('/', progressRoutes);
+
+// Default route
+app.get('/', (req, res) => {
+  res.send('Welcome to the API');
+});
+
+// Set port and listen for our requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`server is running on PORT ${PORT}`);
+})
