c9d982e57bae36e7533eea8b118dbfbab7e4c98e
Yen <a032dsy3371@bangkit.academy>
2023-11-16 22:39:12 +0700

    update CRUD rooms function

--- Backend/src/room/roomController.js
+++ Backend/src/room/roomController.js
@@ -7,54 +7,62 @@ const ref = db.collection('rooms');
 
 exports.insertRoom = async (req, res) => {
     try {
-      const { roomTypeId, hotelName, roomNumber } = req.body;
+        const { roomId, roomTypeId, hotelName, roomNumber } = req.body;
   
-      if (roomTypeId == "") {
-          res.status(http.StatusNoContent).json({ message: 'Failed to insert room. Room type\'s id cannot empty', error: error.message });
-          return
-      }
-      if (hotelName == "") {
-          res.status(http.StatusNoContent).json({ message: 'Failed to insert room. Hotel\'s id cannot empty', error: error.message });
-          return
-      }
-      if (roomNumber == "") {
-          res.status(http.StatusNoContent).json({ message: 'Failed to insert room. Room\'s number cannot empty', error: error.message });
-          return
-      }
+        if (roomId == "") {
+            res.status(http.StatusNoContent).json({ message: 'Failed to insert room. Room\'s id cannot empty', error: error.message });
+            return
+        }
+        if (roomTypeId == "") {
+            res.status(http.StatusNoContent).json({ message: 'Failed to insert room. Room type\'s id cannot empty', error: error.message });
+            return
+        }
+        if (hotelName == "") {
+            res.status(http.StatusNoContent).json({ message: 'Failed to insert room. Hotel\'s id cannot empty', error: error.message });
+            return
+        }
+        if (roomNumber == "") {
+            res.status(http.StatusNoContent).json({ message: 'Failed to insert room. Room\'s number cannot empty', error: error.message });
+            return
+        }
   
-      // Add to firestore
-      const roomData = {
-          roomTypeId: roomTypeId,
-          hotelName: hotelName,
-          roomNumber: roomNumber,
-          status: 'Available',
-      }
-  
-      await ref.add(roomData);
-      res.status(200).json({ message: 'Room inserted successfully', error: null, data: roomData });
+        // Add to firestore
+        const roomData = {
+            roomId: roomId,
+            roomTypeId: roomTypeId,
+            hotelName: hotelName,
+            roomNumber: roomNumber,
+            status: 'Available',
+        }
+    
+        await ref.add(roomData);
+        res.status(200).json({ message: 'Room inserted successfully', error: null, data: roomData });
     } catch (error) {
-      res.status(500).json({ message: 'Failed to insert room', error: error.message });
+        res.status(500).json({ message: 'Failed to insert room', error: error.message });
     }
-  };
+};
 
-  exports.deleteRoom = async (req, res) => {
+exports.deleteRoom = async (req, res) => {
     try {
         const roomId = req.params['roomId']
-        const docRef = ref.doc(roomId)
-        const snapshot = await docRef.get()
-        if (!snapshot.exists) {
+        const snapshot = await ref.where('roomId', '==', roomId).get()
+        if (snapshot.empty) {
             res.status(200).json({ message: 'Room not found', error: null, data: null});
             return;
         }
+        snapshot.forEach(doc => {
+            id = doc.id
+            oldData = doc.data()
+        });
   
-        await docRef.delete();
+        await ref.doc(id).delete();
         res.status(200).json({ message: 'Room deleted successfully', error: null, data: null });
     } catch (error) {
         res.status(401).json({ message: 'Failed to delete room', error: error.message });
     }
-  };
+};
 
-  exports.getRoomsByHotelName = async (req, res) => {
+exports.getRoomsByHotelName = async (req, res) => {
     try {
         const hotelName = req.params['hotelName']
         const snapshot = await ref.where('hotelName', '==', hotelName).get();
@@ -73,4 +81,4 @@ exports.insertRoom = async (req, res) => {
     } catch (error) {
         res.status(401).json({ message: 'Failed to get room', error: error.message });
     }
-  }
+}
