e2eebbdeb68863913ffc444c304e9eadbc837a3a
Stefanus Titan <stefanustitan@gmail.com>
2023-11-30 12:03:01 +0700

    Test

--- Backend/src/room/index.js
+++ Backend/src/room/index.js
@@ -4,11 +4,23 @@ const {
   getRoomByRoomId,
   getRoomsByHotelName,
   searchRoom,
+  insertRoom,
+  updateRoom,
+  deleteRoom,
+  updateRoomStatusToBooked,
+  updateRoomStatusToAvailable,
 } = require("./roomController");
 
 //User
 router.get("/:roomId", getRoomByRoomId);
 router.get("/hotel/:hotelName", getRoomsByHotelName);
 router.get("/", searchRoom);
+
+//Admin
+router.post("/", insertRoom);
+router.put("/:roomId", updateRoom);
+router.put("/:roomId/booked", updateRoomStatusToBooked);
+router.put("/:roomId/available", updateRoomStatusToAvailable);
+router.delete("/:roomId", deleteRoom);
 
 module.exports = router;
