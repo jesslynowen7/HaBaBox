1fb71bed2ab2f577fadc4c6ce21de1a82f0d9586
Stefanus Titan <stefanustitan@gmail.com>
2023-12-01 12:44:32 +0700

    CORS

--- Backend/index.js
+++ Backend/index.js
@@ -1,36 +1,40 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const authRoutes = require('./src/auth');
-const hotelRoutes = require('./src/hotel');
-const roomRoutes = require('./src/room');
-const roomTypeRoutes = require('./src/room_type');
-const transactionRoutes = require('./src/transaction');
-const userRoutes = require('./src/user');
-const eTicketRoutes = require('./src/e_ticket');
-const promoRoutes = require('./src/promo');
+const authRoutes = require("./src/auth");
+const hotelRoutes = require("./src/hotel");
+const roomRoutes = require("./src/room");
+const roomTypeRoutes = require("./src/room_type");
+const transactionRoutes = require("./src/transaction");
+const userRoutes = require("./src/user");
+const eTicketRoutes = require("./src/e_ticket");
+const promoRoutes = require("./src/promo");
+const cors = require("cors");
+
+// Enable CORS for all routes
+app.use(cors());
 
 // Middlewares
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // API routes
-app.use('/auth', authRoutes);
-app.use('/hotel', hotelRoutes);
-app.use('/room', roomRoutes);
-app.use('/room_type', roomTypeRoutes);
-app.use('/transaction', transactionRoutes);
-app.use('/user', userRoutes);
-app.use('/e_ticket', eTicketRoutes);
-app.use('/promo', promoRoutes);
+app.use("/auth", authRoutes);
+app.use("/hotel", hotelRoutes);
+app.use("/room", roomRoutes);
+app.use("/room_type", roomTypeRoutes);
+app.use("/transaction", transactionRoutes);
+app.use("/user", userRoutes);
+app.use("/e_ticket", eTicketRoutes);
+app.use("/promo", promoRoutes);
 // app.use('/', progressRoutes);
 
 // Default route
-app.get('/', (req, res) => {
-  res.send('Welcome to the API');
+app.get("/", (req, res) => {
+  res.send("Welcome to the API");
 });
 
 // Set port and listen for our requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-    console.log(`server is running on PORT ${PORT}`);
-})
+  console.log(`server is running on PORT ${PORT}`);
+});
