7cd1634543f291818dea6a87c1b1c68dbe651129
Stefanus Titan <stefanustitan@gmail.com>
2023-12-01 11:44:55 +0700

    reset index

--- Backend/index.js
+++ Backend/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const express = require("express");
 const app = express();
 <<<<<<< HEAD
@@ -58,3 +59,5 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`server is running on PORT ${PORT}`);
 });
+=======
+>>>>>>> 40e7409 (reset index)
