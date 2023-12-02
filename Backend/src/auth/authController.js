cc92d4097afd2171d2779add06ea943e472ee527
Stefanus Titan <stefanustitan@gmail.com>
2023-12-03 01:58:52 +0700

    getCurrentUserData

--- Backend/src/auth/authController.js
+++ Backend/src/auth/authController.js
@@ -1,11 +1,46 @@
 const firebase = require("firebase/app");
 const { getFirestore } = require("firebase-admin/firestore");
-
 require("../utils/firebase-config.js");
 require("firebase/database");
+const admin = require("firebase-admin");
+const bodyParser = require("body-parser");
 
 const db = getFirestore();
 const ref = db.collection("users");
+
+exports.getCurrentUserData = async (req, res) => {
+  try {
+    const token = req.body.token;
+    admin
+      .auth()
+      .verifyIdToken(token)
+      .then((decodedToken) => {
+        const uid = decodedToken.uid;
+        // Query based on uid
+        ref
+          .where("uid", "==", uid)
+          .get()
+          .then((snapshot) => {
+            // Handle the snapshot here
+            snapshot.forEach((doc) => {
+              id = doc.id;
+              data = doc.data();
+            });
+          })
+          .catch((error) => {
+            console.error("Error getting documents:", error);
+          });
+
+        res.status(200).json({ data });
+      })
+      .catch((error) => {
+        console.error("Error getting user by uid:", error);
+        throw error;
+      });
+  } catch (error) {
+    throw error;
+  }
+};
 
 // Register a new user
 exports.registerUser = async (req, res) => {
@@ -13,8 +48,14 @@ exports.registerUser = async (req, res) => {
     const { name, email, password } = req.body;
 
     // Create a new user in Firebase Authentication
-    await firebase.auth().createUserWithEmailAndPassword(email, password);
+    const userCredential = await firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password);
 
+    // Get the UID (user ID) of the newly created user
+    const userId = userCredential.user.uid;
+
+    // Add user data to Firestore with the same UID as the user in Firebase Authentication
     const userData = {
       name: name,
       email: email,
@@ -22,10 +63,11 @@ exports.registerUser = async (req, res) => {
       profilePic: null,
     };
 
-    await ref.add(userData);
+    // Use the UID as the document ID in Firestore
+    await ref.doc(userId).set(userData);
 
     res
-      .status(201)
+      .status(200)
       .json({ message: "User registered successfully", error: null });
   } catch (error) {
     res
@@ -46,8 +88,20 @@ exports.loginUser = async (req, res) => {
 
     // Generate an access token
     const accessToken = await userCredential.user.getIdToken();
+
+    // Set the access token as an HttpOnly cookie
+    res.cookie("access_token", accessToken, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "strict", // Adjust based on your needs (strict, lax, none)
+      maxAge: 3600000, // Token expiration time in milliseconds (adjust as needed)
+      path: "/", // The path to set the cookie on (adjust based on your application structure)
+    });
+
+    // Fetch additional user data from Firestore
     const emailUser = userCredential.user.email;
     const snapshot = await ref.where("email", "==", emailUser).get();
+
     if (snapshot.empty) {
       res
         .status(200)
@@ -58,6 +112,7 @@ exports.loginUser = async (req, res) => {
       id = doc.id;
       data = doc.data();
     });
+
     res.status(200).json({
       message: "Login successful",
       error: null,
--- Backend/src/auth/index.js
+++ Backend/src/auth/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser, logoutUser } = require('./authController');
+const { registerUser, loginUser, logoutUser, getCurrentUserData } = require('./authController');
 
 // Register user
 router.post('/register', registerUser);
@@ -10,5 +10,8 @@ router.post('/login', loginUser);
 
 // Logout user
 router.post('/logout', logoutUser);
+
+// Get Current User Data
+router.get('/getCurrentUserData', getCurrentUserData);
 
 module.exports = router;
